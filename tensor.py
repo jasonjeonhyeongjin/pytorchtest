125505c926a93d23550763e07061b3234eb2a5a6
Hyeongjin Jeon <hj.jeon@lomin.ai>
2021-04-21 18:50:24 +0900

    add commits in the file

--- tensor.py
+++ tensor.py
@@ -1,11 +1,14 @@
 from __future__ import print_function
 import torch
 
+#초기화되어 있지 않은 3*5 행렬 생성 값은 쓰레기값
 x = torch.empty(5, 3)
 print(x)
 
+#난수로 초기화된 3*5 행렬 생성
 x = torch.rand(5, 3)
 print(x)
 
+#long 타임과 0로 초기화된 3*5 행렬 생성
 x = torch.zeros(5, 3, dtype=torch.long)
 print(x)
